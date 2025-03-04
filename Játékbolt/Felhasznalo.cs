3e7f4fd3662cfbbc775f1d8b35cfc533cd56f2b3
aprobence <aprobence2017@gmail.com>
2025-03-04 08:43:01 +0100

    classok

--- Játékbolt/Felhasznalo.cs
+++ Játékbolt/Felhasznalo.cs
@@ -2,5 +2,40 @@ namespace Játékbolt;
 
 internal class Felhasznalo
 {
-    
+    public string Nev { get; set; }
+    public int Egyenleg { get; set; }
+    public List<Jatek> Konyvtar { get; set; }
+
+    public Felhasznalo(string nev, int egyenleg)
+    {
+        Nev = nev;
+        Egyenleg = egyenleg;
+    }
+
+    public bool Vasarlas(Jatek jatek)
+    {
+        foreach (var j in Konyvtar)
+        {
+            if (j == jatek  | Egyenleg < jatek.Ar)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void KonyvtarMegtekintes()
+    {
+        Console.WriteLine($"{Nev} játékai:");
+        foreach (var j in Konyvtar)
+        {
+            Console.Write($"{j} ");
+        }
+    }
+
+    public void Feltoltes(int osszeg)
+    {
+        Egyenleg += osszeg;
+        Console.WriteLine($"A feltöltés megtörtént, {Egyenleg} összeg van az egyenlegeden.");
+    }
 }
--- Játékbolt/JatekBolt.cs
+++ Játékbolt/JatekBolt.cs
@@ -2,5 +2,46 @@ namespace Játékbolt;
 
 internal class JatekBolt
 {
-    
+    public List<Jatek> Jatekok { get; set; }
+    public List<Felhasznalo> Felhasznalok { get; set; }
+
+    public JatekBolt()
+    {
+        Jatekok = new List<Jatek>();
+        Felhasznalok = new List<Felhasznalo>();
+    }
+
+    public void JatekHozzaadas(Jatek jatek)
+    {
+        Jatekok.Add(jatek);
+    }
+
+    public void FelhasznaloHozzaadas(Felhasznalo felhasznalo)
+    {
+        Felhasznalok.Add(felhasznalo);
+    }
+
+    public Jatek KeresJatek(string cim)
+    {
+        foreach (var j in Jatekok)
+        {
+            if (j.Cim == cim)
+            {
+                return j;
+            }
+        }
+        return null;
+    }
+
+    public Felhasznalo KeresFelhasznalo(string cim)
+    {
+        foreach (var f in Felhasznalok)
+        {
+            if (f.Nev == cim)
+            {
+                return f;
+            }
+        }
+        return null;
+    }
 }
